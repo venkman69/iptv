64ee5dec97dd1e0066a7285ebc11fece8e87226f
Narayan Natarajan <narayan.natarajan@gmail.com>
2025-02-27 17:38:12 -0500

    added a date filter to find latest additions

--- iptv_downloader.py
+++ iptv_downloader.py
@@ -54,19 +54,30 @@ groups = st.session_state.groups
 # with st.sidebar:
 with tab_dl:
     # Create Streamlit widgets for group and language selection, and title search
-    stcol1, stcol2, stcol3=st.columns(3)
+    stcol1, stcol2, stcol3, stcol4=st.columns(4)
     with stcol1:
+        date_picker_toggle=st.toggle("Enable date")
+        if date_picker_toggle:
+            date_added_since = st.date_input("Date added is newer than: ",value="today")
+        else:
+            date_added_since= (1==1)
+        
+    with stcol2:
         providers =["All"] + [rec.provider_m3u_base for rec in iptvdb.IPTVProviderTbl.select()]
         selected_provider = st.selectbox("Select Provider",providers)
-    with stcol2:
-        selected_media_type = st.selectbox("Select Media Type", ["All", "movie", "series", "livetv"])
     with stcol3:
-        where_clause = (1==1)
+        selected_media_type = st.selectbox("Select Media Type", ["All", "movie", "series", "livetv"])
+    with stcol4:
+        # add a datepicker
+
+        where_clauses = [(1==1)]
         if selected_provider != "All":
-            where_clause = (where_clause & ( iptvdb.IPTVTbl.provider_m3u_base == selected_provider) )
+            where_clauses.append(( iptvdb.IPTVTbl.provider_m3u_base == selected_provider) )
         if selected_media_type != "All":
-            where_clause = (where_clause & ( iptvdb.IPTVTbl.media_type == selected_media_type) )
-        groups = [rec.group for rec in iptvdb.IPTVTbl.select(iptvdb.IPTVTbl.group).distinct().where(where_clause)]
+            where_clauses.append(( iptvdb.IPTVTbl.media_type == selected_media_type) )
+        if date_picker_toggle:
+            where_clauses.append((iptvdb.IPTVTbl.added_date >= date_added_since))
+        groups = [rec.group for rec in iptvdb.IPTVTbl.select(iptvdb.IPTVTbl.group).distinct().where(*where_clauses)]
         selected_group = st.selectbox("Select Group", ["All"] + groups)
     
     search_title = st.text_input("Search Title").lower()
@@ -78,9 +89,12 @@ with tab_dl:
         where_clauses.append((iptvdb.IPTVTbl.group == selected_group ) )
     if selected_media_type != "All":
         where_clauses.append((iptvdb.IPTVTbl.media_type == selected_media_type ) )
-    if search_title or selected_group != "All" or selected_media_type != "All" or selected_provider != "All":
+    if date_picker_toggle:
+        where_clauses.append((iptvdb.IPTVTbl.added_date >= date_added_since))
+    where_clauses.append((iptvdb.IPTVTbl.added_date >= date_added_since))
+    if search_title or selected_group != "All" or selected_media_type != "All" or selected_provider != "All" or date_added_since != (1==1):
         print(f"Provider: {selected_provider} selected_group: {selected_group}, search_title: {search_title}")
-        search_cache_key = sha256(f"{selected_provider}{selected_group}{selected_media_type}{search_title}".encode()).hexdigest()
+        search_cache_key = sha256(f"{date_added_since}{selected_provider}{selected_group}{selected_media_type}{search_title}".encode()).hexdigest()
         if st.session_state.get(search_cache_key):
             filtered_media = st.session_state[search_cache_key]
         else:
@@ -91,6 +105,7 @@ with tab_dl:
                 where_clauses.append((iptvdb.IPTVTbl.title.contains(word)))
                 
             filtered_media = iptvdb.IPTVTbl.select().where(*where_clauses).order_by(iptvdb.IPTVTbl.title)
+        
             st.session_state[search_cache_key] = filtered_media
         # display the records in filtered_media as a table
         if not filtered_media:
@@ -119,51 +134,51 @@ with tab_dl:
                                                               "logo":st.column_config.ImageColumn()},
                                                key="search_results")
 
-        selected_items = download_items_df[download_items_df["Download"] == True]
-        if selected_items.empty:
-            st.write("No items selected")
-        else:
-            if "selected_items_details" in st.session_state:
-                # then only fetch items that are missing
-                selected_items_details = st.session_state.selected_items_details
+            selected_items = download_items_df[download_items_df["Download"] == True]
+            if selected_items.empty:
+                st.write("No items selected")
             else:
-                selected_items_details = {}
-            details=[]
-            for item in selected_items.itertuples():
-                if item.url in selected_items_details:
-                    media_info:MyMediaInfo = selected_items_details[item.url]
+                if "selected_items_details" in st.session_state:
+                    # then only fetch items that are missing
+                    selected_items_details = st.session_state.selected_items_details
                 else:
-                    media_info:MyMediaInfo = get_media_info(item.url)
-                    selected_items_details[item.url] = media_info
-                rec={"title":item.original_title}
-                rec.update(media_info.to_dict())
-                details.append(rec)
-            st.session_state["selected_item_details"]=selected_items_details
-            show_details_df = pd.DataFrame(details)
-            st.data_editor(show_details_df)
-
-            if st.button("Add selected items to download queue"):
+                    selected_items_details = {}
+                details=[]
                 for item in selected_items.itertuples():
-                    iptv_obj:iptvdb.IPTVTbl = iptvdb.IPTVTbl.get_or_none(iptvdb.IPTVTbl.url==item.url)
-                    if iptv_obj:
-                        created_date = datetime.now()
-                        download_mgr_obj = iptvdb.DownloadQueueTbl.create(created_date = created_date,
-                                                                          updated_date = created_date,
-                                                                           url = iptv_obj.url,
-                                                                            file_path = iptv_obj.get_target_filename(MOVIE_DOWNLOAD_PATH,SERIES_DOWNLOAD_PATH),
-                                                                             state = iptvdb.DownloadStates.PENDING )
+                    if item.url in selected_items_details:
+                        media_info:MyMediaInfo = selected_items_details[item.url]
                     else:
-                        raise ValueError(f"IPTVTbl object not found for {item.url}")
-                st.write("Submitted dl queue")
+                        media_info:MyMediaInfo = get_media_info(item.url)
+                        selected_items_details[item.url] = media_info
+                    rec={"title":item.original_title}
+                    rec.update(media_info.to_dict())
+                    details.append(rec)
+                st.session_state["selected_item_details"]=selected_items_details
+                show_details_df = pd.DataFrame(details)
+                st.data_editor(show_details_df)
 
-                del st.session_state[search_cache_key]
-                selected_items = download_items_df[download_items_df["Download"] == True]
-                print(download_items_df)
-                try:
-                    for row in selected_items.itertuples():
-                        row["Download"] = False
-                except Exception as e:
-                    print(e)
+                if st.button("Add selected items to download queue"):
+                    for item in selected_items.itertuples():
+                        iptv_obj:iptvdb.IPTVTbl = iptvdb.IPTVTbl.get_or_none(iptvdb.IPTVTbl.url==item.url)
+                        if iptv_obj:
+                            created_date = datetime.now()
+                            download_mgr_obj = iptvdb.DownloadQueueTbl.create(created_date = created_date,
+                                                                            updated_date = created_date,
+                                                                            url = iptv_obj.url,
+                                                                                file_path = iptv_obj.get_target_filename(MOVIE_DOWNLOAD_PATH,SERIES_DOWNLOAD_PATH),
+                                                                                state = iptvdb.DownloadStates.PENDING )
+                        else:
+                            raise ValueError(f"IPTVTbl object not found for {item.url}")
+                    st.write("Submitted dl queue")
+
+                    del st.session_state[search_cache_key]
+                    selected_items = download_items_df[download_items_df["Download"] == True]
+                    print(download_items_df)
+                    try:
+                        for row in selected_items.itertuples():
+                            row["Download"] = False
+                    except Exception as e:
+                        print(e)
 
         st.write("Enter a search title to filter the media list")
 
